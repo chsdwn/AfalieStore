9569d5a9a0219dcb6dd9c40e020d991e68ed5b67
chsdwn <hulusikafalier@gmail.com>
2020-02-21 18:19:27 +0300

    Added mapping to controllers.

--- AfalieStore.API/Controllers/AdminController.cs
+++ AfalieStore.API/Controllers/AdminController.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AfalieStore.Application.ProductsAdmin;
 using AfalieStore.Database;
+using AfalieStore.Domain;
+using AfalieStore.Domain.DTOs;
 using AfalieStore.Domain.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AfalieStore.API.Controllers
@@ -11,37 +15,45 @@ namespace AfalieStore.API.Controllers
     public class AdminController : ControllerBase
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
 
         public AdminController(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
+            _mapper = new Mapper(AutoMapperProfile.config);
         }
 
         [HttpGet("products/{id}")]
         public async Task<IActionResult> GetProduct(int id)
         {
             var product = await new GetProduct(_dbContext).Do(id);
-            return Ok(product);
+            var productToReturn = _mapper.Map<ProductForDetailedAdmin>(product);
+            return Ok(productToReturn);
         }
 
         [HttpGet("products")]
         public async Task<IActionResult> GetProducts()
         {
             var products = await new GetProducts(_dbContext).Do();
-            return Ok(products);
+            var productList = _mapper.Map<IEnumerable<ProductForListAdmin>>(products);
+            return Ok(productList);
         }
 
         [HttpPost("products")]
-        public async Task<IActionResult> CreateProduct([FromBody]Product product)
+        public async Task<IActionResult> CreateProduct([FromBody]ProductForCreationAdmin productForCreationAdminDto)
         {
-            var productToReturn = await new CreateProduct(_dbContext).Do(product);
+            var product = _mapper.Map<Product>(productForCreationAdminDto);
+            var createdProduct = await new CreateProduct(_dbContext).Do(product);
+            var productToReturn = _mapper.Map<ProductForDetailedAdmin>(createdProduct);
             return Ok(productToReturn);
         }
 
         [HttpPut("products/{id}")]
-        public async Task<IActionResult> UpdateProduct([FromBody]Product product, int id)
+        public async Task<IActionResult> UpdateProduct([FromBody]ProductForUpdateAdmin productForUpdateAdminDto, int id)
         {
-            var productToReturn = await new UpdateProduct(_dbContext).Do(product, id);
+            var productToUpdate = _mapper.Map<Product>(productForUpdateAdminDto);
+            var updatedProduct = await new UpdateProduct(_dbContext).Do(productToUpdate, id);
+            var productToReturn = _mapper.Map<ProductForDetailedAdmin>(updatedProduct);
             return Ok(productToReturn);
         }
 
--- AfalieStore.API/Controllers/ProductsController.cs
+++ AfalieStore.API/Controllers/ProductsController.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AfalieStore.Application.Products;
 using AfalieStore.Database;
+using AfalieStore.Domain;
+using AfalieStore.Domain.DTOs;
 using AfalieStore.Domain.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AfalieStore.API.Controllers
@@ -11,17 +15,20 @@ namespace AfalieStore.API.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
 
         public ProductsController(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
+            _mapper = new Mapper(AutoMapperProfile.config);
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var products = await new GetProducts(_dbContext).Do();
-            return Ok(products);
+            var productList = _mapper.Map<IEnumerable<ProductForList>>(products);
+            return Ok(productList);
         }
     }
 }
